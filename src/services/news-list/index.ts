d68c6e496250f4f56269c22233516e7410144b92
Pavel Fomichev <pablodecortes@gmail.com>
2021-06-15 13:37:22 +0200

    move axios setup to separate file

--- src/services/news-list/index.ts
+++ src/services/news-list/index.ts
@@ -1,12 +1,6 @@
-import axios from "axios";
 import { NewsItem } from "pages/types";
-import Qs from "qs";
 import { useState, useEffect } from "react";
-
-axios.defaults.baseURL = "http://localhost:3000";
-axios.defaults.paramsSerializer = (params) => {
-  return Qs.stringify(params, { arrayFormat: "repeat" });
-};
+import { client } from 'api/axios';
 
 export const useNewsList = (tags?: string[]) => {
   const [news, setNews] = useState<NewsItem[]>([]);
@@ -15,7 +9,7 @@ export const useNewsList = (tags?: string[]) => {
     const fetchData = async () => {
       try {
         const request = !tags ? undefined : { params: { tags } };
-        const result = await axios.get<NewsItem[]>("/api/news", request);
+        const result = await client.get<NewsItem[]>("/api/news", request);
         setNews(result.data);
       } catch (error) {
         setNews([]);
