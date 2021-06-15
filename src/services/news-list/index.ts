fdfef01c92f6f654215d4bffb643883b35157656
Pavel Fomichev <pablodecortes@gmail.com>
2021-06-15 13:39:55 +0200

    fix request body

--- src/services/news-list/index.ts
+++ src/services/news-list/index.ts
@@ -1,6 +1,6 @@
 import { NewsItem } from "pages/types";
 import { useState, useEffect } from "react";
-import { client } from 'api/axios';
+import { client } from "api/axios";
 
 export const useNewsList = (tags?: string[]) => {
   const [news, setNews] = useState<NewsItem[]>([]);
@@ -8,13 +8,14 @@ export const useNewsList = (tags?: string[]) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const request = !tags ? undefined : { params: { tags } };
-        const result = await client.get<NewsItem[]>("/api/news", request);
+        const result = await client.get<NewsItem[]>("/api/news", {
+          params: { tags: tags ?? [] },
+        });
         setNews(result.data);
       } catch (error) {
         setNews([]);
       }
-    }
+    };
 
     fetchData();
   }, [tags]);
