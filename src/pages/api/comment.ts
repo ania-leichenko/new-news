aff61c9798d3bbf1aae4fad50ea8c125ed460f82
Anna Leichenko <ania.leichenko@gmail.com>
2021-07-21 17:41:07 +0300

    removed unnecessary

--- src/pages/api/comment.ts
+++ src/pages/api/comment.ts
@@ -3,10 +3,6 @@ import db from "db";
 import { getSession } from "next-auth/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  //   console.log(req.body);
-  //   const item = {
-  //     comments: [req.body.comment],
-  //   };
   const item = db.news.findOne({ _id: req.body.id });
 
   item.comments.push(req.body.comment);
@@ -14,6 +10,3 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   db.news.update({ _id: req.body.id }, { comments: item.comments });
   res.status(200).json({});
 };
-
-// const arr = ["c", { name: "a" }, { name: "b" }];
-// console.log(arr[1].name);
